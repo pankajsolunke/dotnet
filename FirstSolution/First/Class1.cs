55be315e39c19fa3af198eee89452831e1a38841
Pankaj D <pankajsolunke8506@gmail.com>
2025-02-14 08:11:42 +0530

    add a .exe file and Firstsolution file

---
+++ FirstSolution/First/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+     class Class1
+    {
+        /*
+         Constructor: 
+         1.-> It's a special method present under a class
+         responsible for initializing the variables of that class.
+
+         2.-> The name of a constructor method is exactly
+         the same name of the class in which it was present and 
+         more over it's a non-value returning method.
+
+         3.-> Each and every class requires this constructor if we
+          want to create the instance of that class. 
+
+         I.-> Implicitly defined constructor are public.
+          
+         4.-> Implicitly defined constructors are parameter less and these 
+          constructors are also known as default constructors.
+
+         II.-> Explicit constructor can be parameter less or parameterized also.
+         */
+
+        //int a, b;
+        static void Main(string[] args)
+        {
+            Class1 f = new Class1 ();
+
+            //Console.WriteLine("Value of a is: "+ f.a);
+            //Console.WriteLine("Value of b is: "+ f.b);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FirstSolution/First/Explicit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    class Explicit
+    {
+        public Explicit() {
+
+            Console.WriteLine("Constructor is called.");
+        }
+        static void Main(string[] args)
+        {
+            Explicit e = new Explicit();
+            Explicit e1 = new Explicit();
+            Explicit e2= new Explicit();
+            Console.ReadLine();
+        }
+    }
+}
