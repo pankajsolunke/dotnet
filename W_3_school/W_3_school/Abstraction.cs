ca38c05b4e5b658c92cf22ade045ef815f6c9922
Pankaj D <pankajsolunke8506@gmail.com>
2025-02-27 16:13:33 +0530

    modify the abstraction class

--- W_3_school/W_3_school/Abstraction.cs
+++ W_3_school/W_3_school/Abstraction.cs
@@ -6,17 +6,28 @@ using System.Threading.Tasks;
 
 namespace W_3_school
 {
+    // Abstract class
     abstract class Animals
     {
+        // Abstract method (does not have a body)
         public abstract void animalS();
-       
-       
-        
+
+        // Regular Method
         public void sleep()
         {
-            Console.WriteLine("ZzZ");
+            Console.WriteLine("good night ZzZ");
         }
 
+    }
+
+    //Derived Class (Inherit from Animals)
+    class Cat : Animals
+    {
+        public override void animalS()
+        {
+            //The Body of animalS is provided here
+            Console.WriteLine("The cat says: meow meow");
+        }
     }
     class Abstraction
     {
@@ -25,6 +36,10 @@ namespace W_3_school
             // it is not possible to create an object of the Animal class
             // Cannot create an instance of the abstract class or interface
             // Animals a = new Animals();
+
+            Cat c = new Cat(); // Create a cat object
+            c.animalS();  // Call the Abstract method
+            c.sleep();   // Call the Regular method
         }
 
     }
