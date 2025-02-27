9de246e30275ec8a6bc05e102d60ee7efefe43c5
Pankaj D <pankajsolunke8506@gmail.com>
2025-02-27 16:59:05 +0530

    create a new class file is Multiple Interface

---
+++ W_3_school/W_3_school/MInterface.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W_3_school
+{
+    // To implement multiple interfaces, separate them with a comma( , )
+    interface IFirst
+    {
+        void First();// interface method
+    }
+    interface ISecond
+    {
+        void Second(); // interface method
+    }
+
+    // Implement multiple interfaces
+    class DemoClass : IFirst , ISecond
+    {
+        public void First()
+        {
+            Console.WriteLine("First Interface...");
+        }
+        public void Second()
+        {
+            Console.WriteLine("Second Interface...");
+        }
+    }
+    class MInterface
+    {
+        static void Main(string[] args)
+        {
+            DemoClass dc = new DemoClass();
+            dc.First();
+            dc.Second();
+        }
+    }
+}
